dd143d71e47e178d69f99c84a9056134ca8cc5db
patiltanush14-glitch <patil.tanush14@gmail.com>
2026-03-30 00:40:19 +0530

    Update app.py

--- resume-analyzer/app.py
+++ resume-analyzer/app.py
@@ -35,6 +35,4 @@ def index():
 
 
 if __name__ == "__main__":
-    if not os.path.exists("uploads"):
-        os.makedirs("uploads")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
